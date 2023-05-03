b2009708e82facd03dda82f0a1d7c10024747f45
patrykstruzek <72736082+patrykstruzek@users.noreply.github.com>
2023-05-03 21:50:15 +0200

    Attempting to repair files

--- Source/GameProject2D3D/MainActor.cpp
+++ Source/GameProject2D3D/MainActor.cpp
@@ -1,11 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 
 #include "MainActor.h"
-<<<<<<< HEAD
 #include "MainCharacter.h"
-=======
->>>>>>> e36d8d907f614132a2f3ab55b3e8c52876dcc8dd
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
 #include "GameFramework/SpringArmComponent.h"
@@ -14,10 +11,7 @@
 #include "EnhancedInputSubsystems.h"
 #include "Components/CapsuleComponent.h"
 
-<<<<<<< HEAD
-// Sets default values
-=======
->>>>>>> e36d8d907f614132a2f3ab55b3e8c52876dcc8dd
+
 AMainActor::AMainActor()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
@@ -35,13 +29,10 @@ AMainActor::AMainActor()
 
 	Flipbook = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("Sprite"));
 	Flipbook->SetupAttachment(GetCapsuleComponent());
-<<<<<<< HEAD
 	Flipbook->SetRelativeLocation(FVector(7.0f, 0.0f, -20.0f));
-
-=======
 	Flipbook->SetRelativeLocation(FVector(7.0f,0.0f,-20.0f));
 	
->>>>>>> e36d8d907f614132a2f3ab55b3e8c52876dcc8dd
+
 	CameraHandle = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraHandle"));
 	CameraHandle->SetupAttachment(GetCapsuleComponent());
 	CameraHandle->SetRelativeRotation(FRotator(-30.0f, -90.0f, 0.0f));
@@ -87,7 +78,6 @@ void AMainActor::SetMappingContext()
 	}
 }
 
-<<<<<<< HEAD
 void AMainActor::Move(const FInputActionValue& Value)
 {
 	FVector2D MovementVector = Value.Get<FVector2D>();
@@ -181,7 +171,6 @@ void AMainActor::PlaySlash()
 	Flipbook->SetFlipbook(Idle);
 }
 
-=======
 // Called when the game starts or when spawned
 void AMainActor::BeginPlay()
 {
@@ -203,7 +192,6 @@ void AMainActor::Tick(float DeltaTime)
 }
 
 // Called to bind functionality to input
->>>>>>> e36d8d907f614132a2f3ab55b3e8c52876dcc8dd
 void AMainActor::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
